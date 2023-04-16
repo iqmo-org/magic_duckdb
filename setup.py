9b3d5e903d539a63bf5d6941f1f1d3e7f355f514
Paul Timmins <paul@iqmo.com>
2023-04-16 18:49:25 -0400

    Move version bumping to dunmai

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
+from dunamai import Version
 
 here = path.abspath(path.dirname(__file__))
 
@@ -9,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="magic_duckdb",
-    version="0.1.16.dev2",
+    version=Version.from_any_vcs().serialize(),
     description="Jupyter Cell and Line Magics for DuckDB",
     long_description=long_description,
     long_description_content_type="text/markdown",
