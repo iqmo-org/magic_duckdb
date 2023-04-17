a5ae05d154e35e3282f646d858a64b3f5884a0df
Paul Timmins <paul@iqmo.com>
2023-04-17 08:15:41 -0400

    Insert git commit into _version.py, and add to init

--- magic_duckdb/__init__.py
+++ magic_duckdb/__init__.py
@@ -1,1 +1,4 @@
 from magic_duckdb.magic import load_ipython_extension  # noqa
+from magic_duckdb._version import __version__
+from magic_duckdb._version import __commit__
+from magic_duckdb._version import __commit_short__
--- magic_duckdb/_version.py
+++ magic_duckdb/_version.py
@@ -1,1 +1,8 @@
-__version__ = "PLACEHOLDER"
+# This file is entirely overwritten at build time
+# Module dunamai is used to create the dev and release __version__'s
+# from the most recent git tag
+# See git action yaml for details
+
+__version__ = "0.0.0"
+__commit__ = "1234567890"
+__commit_short__ = "1234"
